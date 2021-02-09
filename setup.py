f009cafe5eecbe04413af89c87aad6c0cb351c7c
Simon Legner <Simon.Legner@gmail.com>
2021-02-09 22:22:10 +0100

    Configure setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='pyAvaCore',
+    version='0.0.0',
+    url='https://gitlab.com/albina-euregio/pyAvaCore',
+    license='GPL',
+    author='Friedrich Mütschele',
+    packages=find_packages(exclude=['tests']),
+)
