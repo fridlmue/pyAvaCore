0cbbd8a27904d9888b078cbe0d06fc73a6b8dffd
fridlmue <fridl@10hoch-6.de>
2021-02-10 08:41:24 +0000

    Add test vorarlberg

--- pyAvaCore/pyAvaCoreSwiss.py
+++ pyAvaCore/pyAvaCoreSwiss.py
@@ -1,20 +1,18 @@
 """
-    Copyright (C) 2020 Friedrich Mütschele and other contributors
-    This file is part of avaRisk.
-    avaRisk is free software: you can redistribute it and/or modify
+    Copyright (C) 2021 Friedrich Mütschele and other contributors
+    This file is part of pyAvaCore.
+    pyAvaCore is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    avaRisk is distributed in the hope that it will be useful,
+    pyAvaCore is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with avaRisk. If not, see <http://www.gnu.org/licenses/>.
+    along with pyAvaCore. If not, see <http://www.gnu.org/licenses/>.
 """
-"""
-    The Python Code is responsible for correct fetch of the slf avalanche information.
-"""
+
 import threading
 from datetime import datetime
 
---
+++ tests/test_vorarlberg.py
@@ -0,0 +1,24 @@
+from pyAvaCore import pyAvaCore
+import unittest
+import xml.etree.ElementTree as ET
+
+class TestVorarlberg(unittest.TestCase):
+
+    def test_albina(self):
+        root = ET.parse(f'{__file__}.xml')
+        reports = pyAvaCore.parseXMLVorarlberg(root)
+        self.assertEqual(len(reports), 7)
+        report = reports[2]
+        self.assertEqual(report.repDate.isoformat(), '2021-02-09T07:30:00')
+        self.assertEqual(report.timeBegin.isoformat(), '2021-02-09T07:30:00')
+        self.assertEqual(report.timeEnd.isoformat(), '2021-02-10T07:30:00')
+        self.assertIn('AT8R3', report.validRegions)
+        self.assertNotIn('AT8R1', report.validRegions)
+        self.assertEqual(report.dangerMain[0]['mainValue'], 3)
+        self.assertEqual(report.dangerMain[0]['validElev'], 'ElevationRange_2200Hi')
+        # self.assertEqual(report.dangerPattern, ['DP6', 'DP2'])
+        self.assertEqual(report.problemList[0]['type'], 'drifting snow')
+        self.assertEqual(report.problemList[1]['type'], 'old snow')
+
+if __name__ == '__main__':
+    unittest.main()
