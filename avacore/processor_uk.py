5a691cc26ff0e93f8b305588bec44ce43aaa3cfb
fridlmue <fridl@10hoch-6.de>
2021-12-02 12:56:16 +0100

    change to new json format

--- avacore/processor_uk.py
+++ avacore/processor_uk.py
@@ -1,11 +1,13 @@
 import json
 import urllib.request
-import datetime
+from datetime import datetime
 from datetime import timedelta
+import pytz
+import dateutil.parser
 
-from avacore import pyAvaCore
+from avacore.avabulletin import AvaBulletin, DangerRatingType, AvalancheProblemType, AvaCoreCustom, ElevationType, RegionType
 
-def process_reports_uk(today=datetime.datetime.today().date()):
+def process_reports_uk(today=datetime.today().date()):
     reports = []
 
     url = 'https://www.sais.gov.uk/api?action=getForecast' # &params=" + region_id[3:]
@@ -21,43 +23,54 @@ def process_reports_uk(today=datetime.datetime.today().date()):
     sais_reports = json.loads(content)
 
     for sais_report in sais_reports:
-        report = pyAvaCore.AvaReport()
-        report.valid_regions.append('UK-' + sais_report['Region'])
-        report.report_id = 'UK-' + sais_report['ID']
+        report = AvaBulletin()
+        # report.valid_regions.append('UK-' + sais_report['Region'])
+        report.regions.append(RegionType('UK-' + sais_report['Region']))
+        report.bulletinID = 'UK-' + sais_report['ID']
 
-        report.rep_date = datetime.datetime.fromisoformat(sais_report['DatePublished']) # 18:00
-        report.rep_date = report.rep_date.replace(hour=18)
-        report.validity_begin = report.rep_date
-        report.validity_end = report.validity_begin + timedelta(days=1)
-        report.report_texts.append(pyAvaCore.ReportText('activity_hl', sais_report['Summary']))
-        report.report_texts.append(pyAvaCore.ReportText('activity_com', 'Forecast Snow Stability: ' + sais_report['SnowStability']))
-        report.report_texts.append(pyAvaCore.ReportText('snow_struct_com', \
-            'Forecast Weather Influences: ' + sais_report['WeatherInfluences'] +\
+        report.publicationTime = dateutil.parser.parse(sais_report['DatePublished']) # 18:00
+        report.validTime.startTime = report.publicationTime.replace(hour=18)
+        report.validTime.endTime = report.validity_begin + timedelta(days=1)
+        report.avalancheActivityHighlights = sais_report['Summary']
+        report.avalancheActivityComment = 'Forecast Snow Stability: ' + sais_report['SnowStability']
+        report.snowpackStructureComment = 'Forecast Weather Influences: ' + sais_report['WeatherInfluences'] +\
             '\n' + 'Observed Weather Influences: ' + sais_report['ObservedWeatherInfluences'] +\
-            '\n' + 'ObservedSnowStability: ' + sais_report['ObservedSnowStability']))
+            '\n' + 'ObservedSnowStability: ' + sais_report['ObservedSnowStability']
 
         problems = int(sais_report['KeyIcons'])
 
-        if problems & (1<<1):
-            problem_type = 'drifting snow'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
-        if problems & (1<<2):
-            problem_type = 'old snow'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
-        if problems & (1<<3):
-            problem_type = 'new snow'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
-        if problems & (1<<4):
-            problem_type = 'wet snow'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
-        if problems & (1<<5):
-            problem_type = 'Cornices'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
-        if problems & (1<<6):
-            problem_type = 'gliding snow'
-            report.problem_list.append(pyAvaCore.Problem(problem_type, [], ''))
+        problem = AvalancheProblemType()
 
-        report.danger_main.append(pyAvaCore.DangerMain(max(sais_report['CompassRose'][4:36]), '-'))
+        if problems & (1<<1):
+            problem = AvalancheProblemType()
+            problem.problemType = 'wind_drifted_snow'
+            report.avalancheProblems.append(problem)
+        if problems & (1<<2):
+            problem = AvalancheProblemType()
+            problem.problemType = 'persistent_weak_layers'
+            report.avalancheProblems.append(problem)
+        if problems & (1<<3):
+            problem = AvalancheProblemType()
+            problem.problemType = 'new_snow'
+            report.avalancheProblems.append(problem)
+        if problems & (1<<4):
+            problem = AvalancheProblemType()
+            problem.problemType = 'wet_snow'
+            report.avalancheProblems.append(problem)
+        if problems & (1<<5):
+            problem = AvalancheProblemType()
+            #problem.problemType = 'cornice_failure'
+            report.avalancheProblems.append(problem)
+        if problems & (1<<6):
+            problem = AvalancheProblemType()
+            problem.problemType = 'gliding_snow'
+            report.avalancheProblems.append(problem)
+
+        # report.danger_main.append(pyAvaCore.DangerMain(max(sais_report['CompassRose'][4:36]), '-'))
+
+        danger_rating = DangerRatingType()
+        danger_rating.set_mainValue_int(max(sais_report['CompassRose'][4:36]))
+        report.dangerRatings.append(danger_rating)
 
         reports.append(report)
 
