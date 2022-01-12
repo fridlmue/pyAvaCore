21c6525c3e9b39f224c29fd2d25b3454f475ad86
fridlmue <fridl@10hoch-6.de>
2022-01-12 12:44:33 +0100

    first attempt to process IS report

--- avacore/avabulletin.py
+++ avacore/avabulletin.py
@@ -201,9 +201,9 @@ class AvalancheProblemType:
         if not '_' in problem_type_text:
             if 'new' in problem_type_text:
                 problem_type_text = 'new_snow'
-            elif 'drifting'in problem_type_text:
+            elif 'drifting'in problem_type_text or 'drifted' in problem_type_text:
                 problem_type_text = 'wind_drifted_snow'
-            elif 'old' in problem_type_text:
+            elif 'old' in problem_type_text or 'persistent':
                 problem_type_text = 'persistent_weak_layers'
             elif 'wet' in problem_type_text:
                 problem_type_text = 'wet_snow'
---
+++ avacore/processor_is.py
@@ -0,0 +1,114 @@
+"""
+    Copyright (C) 2022 Friedrich Mütschele and other contributors
+    This file is part of pyAvaCore.
+    pyAvaCore is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    pyAvaCore is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with pyAvaCore. If not, see <http://www.gnu.org/licenses/>.
+"""
+from datetime import datetime
+from datetime import timezone
+from datetime import time
+from datetime import timedelta
+import pytz
+import logging
+import typing
+import copy
+import re
+import dateutil.parser
+from urllib.request import urlopen, Request
+
+from avacore import pyAvaCore
+from avacore.avabulletin import AvaBulletin, DangerRatingType, AvalancheProblemType, RegionType
+
+def download_report_is(lang):
+    try:
+        import xml.etree.cElementTree as ET
+    except ImportError:
+        import xml.etree.ElementTree as ET
+
+    req = Request('https://xmlweather.vedur.is/avalanche?op=xml&type=status&lang='+lang) # lang can only be `is` or `en`
+    logging.info('Fetching %s', req.full_url)
+    response_content = urlopen(req).read()
+
+    try:
+        root = ET.fromstring(response_content.decode('utf-8'))
+    except Exception as r_e:
+        print('error parsing ElementTree: ' + str(r_e))
+
+    return root
+
+def process_reports_is(path='', cached=False, lang='en'):
+    try:
+        import xml.etree.cElementTree as ET
+    except ImportError:
+        import xml.etree.ElementTree as ET
+
+    if not cached:
+        root = download_report_is(lang)
+        
+    else:
+        root = ET.parse(path)
+
+    common_report = AvaBulletin()
+    
+    conditions = root.find('conditions')
+    common_report.travelAdvisoryHighlights = conditions.find('short_description').text
+    common_report.travelAdvisoryComment = re.sub('(\<.*?\>)', '', conditions.find('full_description').text)
+    common_report.publicationTime = pytz.timezone("Iceland").localize(dateutil.parser.parse(conditions.find('update_time').text))
+    
+    weather_forecast = root.find('weather_forecast')
+    common_report.wxSynopsisComment = weather_forecast.find('forecast').text
+    
+    reports = []
+    
+    area_forecasts = root.find('area_forecasts')
+    for area_forcast in area_forecasts.iter(tag='area_forecast'):
+        report = copy.deepcopy(common_report)
+        report.publicationTime = pytz.timezone("Iceland").localize(dateutil.parser.parse(area_forcast.find('updated').text))
+        report.validTime.startTime = pytz.timezone("Iceland").localize(dateutil.parser.parse(area_forcast.find('valid_from').text))
+        report.validTime.endTime = pytz.timezone("Iceland").localize(dateutil.parser.parse(area_forcast.find('valid_until').text))
+        report.regions.append(RegionType(area_forcast.find('region_code').text, area_forcast.find('region').text))
+        
+        report.bulletinID = report.regions[0].regionID + '-' + report.publicationTime.isoformat()
+        
+        report.avalancheActivityHighlights = area_forcast.find('forecast').text
+        report.avalancheActivityComment = area_forcast.find('recent_avalances').text
+        report.snowpackStructureHighlights = area_forcast.find('snow_condition').text
+        report.wxSynopsisHighlights = area_forcast.find('weather').text
+        
+        danger_rating = DangerRatingType()
+        danger_rating.set_mainValue_int(int(area_forcast.find('danger_level_day1_code').text))
+        report.dangerRatings.append(danger_rating)
+        
+        for snow_problem in area_forcast.iter(tag='snow_problem'):
+            problem_danger_rating = DangerRatingType()
+            
+            aspects_list = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']*2
+            index_from = '0'
+            index_to = '0'
+            index_from = aspects_list.index(snow_problem.find('aspect_from').text)
+            index_to = aspects_list.index(snow_problem.find('aspect_to').text)
+            problem_danger_rating.aspect = aspects_list[index_from:index_to+1]
+            
+            if snow_problem.find('height').text != '0':
+                up_down = '>'
+                if 'Above' in snow_problem.find('height').text:
+                    up_down = '<'
+                problem_danger_rating.elevation.auto_select(up_down + snow_problem.find('height').text)
+            
+            problem = AvalancheProblemType()
+            problem.add_problemType(snow_problem.find('type').text)
+            problem.dangerRating = problem_danger_rating
+            report.avalancheProblems.append(problem)
+        
+        reports.append(report)
+        
+    return reports
+    
