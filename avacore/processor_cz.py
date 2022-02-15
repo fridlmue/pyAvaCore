d6b7a1438c08f3144a0815da04de4c38b4c3c394
fridlmue <fridl@10hoch-6.de>
2022-02-15 10:59:14 +0100

    cleanup

--- avacore/processor_cz.py
+++ avacore/processor_cz.py
@@ -25,7 +25,6 @@ import logging
 from avacore.avabulletin import AvaBulletin, DangerRatingType, AvalancheProblemType, AvaCoreCustom, ElevationType, RegionType
 
 def process_reports_cz():
-
     url = "https://www.horskasluzba.cz/cz/avalanche-json"
 
     headers = {
@@ -46,13 +45,6 @@ def process_reports_cz():
 
 def get_reports_fromjson(cz_report, fetch_time_dependant=True):
     reports = []
-    
-    '''
-    current = 0
-    now = datetime.now(pytz.timezone('Europe/Prague'))
-    if fetch_time_dependant and now.time() > time(17, 0, 0):
-        current = 1
-    '''
 
     for bulletin in cz_report:
         report = AvaBulletin()
@@ -87,67 +79,3 @@ def get_reports_fromjson(cz_report, fetch_time_dependant=True):
         reports.append(report)
 
     return reports
-    
-    '''
-    report.regions.append(RegionType(region_id))
-    report.publicationTime = dateutil.parser.parse(report[current]['PublishTime'].split('.')[0])
-    report.bulletinID = (region_id + "_" + str(report.publicationTime))
-
-    report.validTime.startTime = dateutil.parser.parse(report[current]['ValidFrom'])
-    report.validTime.endTime = dateutil.parser.parse(report[current]['ValidTo'])
-
-    # report.danger_main.append(pyAvaCore.DangerMain(int(varsom_report[current]['DangerLevel']), '-'))
-    
-    danger_rating = DangerRatingType()
-    danger_rating.set_mainValue_int(int(report[current]['DangerLevel']))
-    
-    report.dangerRatings.append(danger_rating)
-
-    for problem in report[current]['AvalancheProblems']:
-        problem_type = ''
-        if problem['AvalancheProblemTypeId'] == 7:
-            problem_type = 'new_snow'
-        elif problem['AvalancheProblemTypeId'] == 10:
-            problem_type = 'wind_drifted_snow'
-        elif problem['AvalancheProblemTypeId'] == 30:
-            problem_type = 'persistent_weak_layers'
-        elif problem['AvalancheProblemTypeId'] == 45:
-            problem_type = 'wet_snow'
-        elif problem['AvalancheProblemTypeId'] == 0: #???
-            problem_type = 'gliding_snow'
-        elif problem['AvalancheProblemTypeId'] == 0: #???
-            problem_type = 'favourable_situation'
-
-        aspects = ['N','NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
-        aspect_list = []
-
-        for i, c in enumerate(problem['ValidExpositions']):
-            if c == '1':
-                aspect_list.append(aspects[i])
-
-        elev_prefix = ''
-        if problem['ExposedHeightFill'] == 1:
-            elev_prefix = '>'
-        elif problem['ExposedHeightFill'] == 2:
-            elev_prefix = '<'
-        
-        if not problem_type == '':
-            problem_danger_rating = DangerRatingType()
-            problem_danger_rating.aspect = aspect_list
-            problem_danger_rating.elevation.auto_select(elev_prefix + str(problem['ExposedHeight1']))
-            problem = AvalancheProblemType()
-            problem.dangerRating = problem_danger_rating
-            problem.problemType = problem_type
-            report.avalancheProblems.append(problem)
-
-    report.avalancheActivityHighlights = report[current]['MainText']
-    report.avalancheActivityComment = report[current]['AvalancheDanger']
-    waek_layers = ''
-    if report[0]['CurrentWeaklayers'] != None:
-        waek_layers = '\n' + report[0]['CurrentWeaklayers']
-    report.snowpackStructureComment = report[current]['SnowSurface']  + waek_layers
-    report.tendency.tendencyComment = report[current+1]['MainText']
-
-
-    
-    '''
