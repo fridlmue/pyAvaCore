1056117e7415e89dc99ce2663b30272bc24f484d
fridlmue <fridl@10hoch-6.de>
2022-02-28 14:18:37 +0100

    improve

--- avacore/processor_es.py
+++ avacore/processor_es.py
@@ -57,8 +57,19 @@ def get_reports_from_file(aemet_reports):
     report.validTime.endTime = report.publicationTime + timedelta(hours=24)
     t_spain = dateutil.parser.parse(re_result.group(0)[1:-1], fuzzy=True)
     t_spain = pytz.timezone("Europe/Madrid").localize(t_spain)
-    # Gültig 24h
-
+    
+    re_result = re.search('(?<=2\.- Estado del manto y observaciones recientes:)(?s:.*)(?=3\.- Evolución del manto)', aemet_reports)
+    report.snowpackStructureComment = ' '.join(re_result.group(0).splitlines()[1:])
+    
+    re_result = re.search('(?<=3\.- Evolución del manto y peligro)(?s:.*)(?=4.- Predicción meteorológica)', aemet_reports)
+    report.avalancheActivityComment = ' '.join(re_result.group(0).splitlines()[2:])
+    
+    re_result = re.search('(?<=4\.- Predicción meteorológica)(?s:.*)(?=5\.- Avance para)', aemet_reports)
+    report.wxSynopsisComment = ' '.join(re_result.group(0).splitlines()[1:])
+    
+    re_result = re.search('(?<=5\.- Avance para el)(?s:.*)(?=</TXT_PREDICCION>)', aemet_reports)
+    report.tendency.tendencyComment = ' '.join(re_result.group(0).splitlines()[1:])
+    
     re_result = re.search('(?<=1\.- Estimación del nivel de peligro:)(?s:.*)(?=2\.- Estado del manto y observaciones recientes)', aemet_reports)
     levels = re_result.group(0).splitlines()
     
@@ -86,14 +97,23 @@ def get_reports_from_file(aemet_reports):
             pm_sent = False
             if len(sentence) > 1:
                 danger_rating = DangerRatingType()
+                danger_rating2 = None
                 levels = re.findall(r"\((.)\)", sentence)
-                if len(levels) > 1:
+                if len(levels) > 1 and not (('por debajo' in sentence) and ('por encima' in sentence)):
+                    print('pm', len(levels) > 1, ('por debajo' in sentence), ('por encima' in sentence))
                     pm_sent = True
                     pm = True
                 if 'por debajo' in sentence:
                     danger_rating.elevation.upperBound = re.findall(r"(\d+) m", sentence)[0]
                     if pm_sent:
                         pm_ratings_lw = int(levels[1])
+                    if 'por encima' in sentence:
+                        danger_rating2 = DangerRatingType()
+                        danger_rating2.elevation.lowerBound = re.findall(r"(\d+) m", sentence)[0]
+                        '''
+                        if pm_sent:
+                            pm_ratings_hi = int(levels[1])
+                        '''
                 elif 'por encimade' in sentence:
                     danger_rating.elevation.lowerBound = re.findall(r"(\d+) m", sentence)[0]
                     if pm_sent:
@@ -102,6 +122,11 @@ def get_reports_from_file(aemet_reports):
                     pm_ge = int(levels[1])
                 danger_rating.set_mainValue_int(int(levels[0]))
                 current_report.dangerRatings.append(danger_rating)
+                
+                if danger_rating2 is not None:
+                    danger_rating2.set_mainValue_int(int(levels[1]))
+                    current_report.dangerRatings.append(danger_rating2)
+                    
                 
         if pm:
             pm_report = copy.deepcopy(current_report)
