46b9eadf202715d284d999067a1a19983b10dec0
fridlmue <fridl@10hoch-6.de>
2022-03-02 11:32:55 +0100

    Add Tests

--- avacore/processor_es.py
+++ avacore/processor_es.py
@@ -22,28 +22,37 @@ import pytz
 import dateutil.parser
 import re
 import copy
+import calendar
+import locale
 
 from avacore import pyAvaCore
 from avacore.avabulletin import AvaBulletin, DangerRatingType, AvalancheProblemType, RegionType
 
 code_dir = {
-    'SOBRARBE': 'ES-SO',
+    'SOBRARBE' : 'ES-SO',
     'RIBAGORZA': 'ES-RI',
     'JACETANIA': 'ES-JA',
-    'GÁLLEGO': 'ES-GA',
-    'NAVARRA': 'ES-NA'
+    'GÁLLEGO'  : 'ES-GA',
+    'NAVARRA'  : 'ES-NA'
 }
+
+# https://stackoverflow.com/questions/19927654/using-dateutil-parser-to-parse-a-date-in-another-language/62581811#62581811
+class LocaleParserInfo(dateutil.parser.parserinfo):
+    locale.setlocale(locale.LC_ALL, 'es_ES')
+    WEEKDAYS = zip(calendar.day_abbr, calendar.day_name)
+    MONTHS = list(zip(calendar.month_abbr, calendar.month_name))[1:]
+    locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
 
 def process_reports_es(today=datetime.today().date(), lang='es'):
     url = 'http://www.aemet.es/xml/montana/p18tarn1.xml'
-    
+
     req = urllib.request.Request(url)
 
     with urllib.request.urlopen(req) as response:
         bulletin_raw = response.read()
-    
+
     reports = get_reports_from_file(bulletin_raw.decode("ISO-8859-1"))
-    
+
     return reports
 
 def get_reports_from_file(aemet_reports):
@@ -51,28 +60,29 @@ def get_reports_from_file(aemet_reports):
     report = AvaBulletin()
 
     re_result = re.search('(?<=Día)(.*)(?=hora oficial)', aemet_reports)
-    t_spain = dateutil.parser.parse(re_result.group(0)[1:-1], fuzzy=True)
+
+    t_spain = dateutil.parser.parse(re_result.group(0)[1:-1], fuzzy=True, parserinfo=LocaleParserInfo())
     report.publicationTime = pytz.timezone("Europe/Madrid").localize(t_spain)
     report.validTime.startTime = report.publicationTime
     report.validTime.endTime = report.publicationTime + timedelta(hours=24)
     t_spain = dateutil.parser.parse(re_result.group(0)[1:-1], fuzzy=True)
     t_spain = pytz.timezone("Europe/Madrid").localize(t_spain)
-    
+
     re_result = re.search('(?<=2\.- Estado del manto y observaciones recientes:)(?s:.*)(?=3\.- Evolución del manto)', aemet_reports)
     report.snowpackStructureComment = ' '.join(re_result.group(0).splitlines()[1:])
-    
+
     re_result = re.search('(?<=3\.- Evolución del manto y peligro)(?s:.*)(?=4.- Predicción meteorológica)', aemet_reports)
     report.avalancheActivityComment = ' '.join(re_result.group(0).splitlines()[2:])
-    
+
     re_result = re.search('(?<=4\.- Predicción meteorológica)(?s:.*)(?=5\.- Avance para)', aemet_reports)
     report.wxSynopsisComment = ' '.join(re_result.group(0).splitlines()[1:])
-    
+
     re_result = re.search('(?<=5\.- Avance para el)(?s:.*)(?=</TXT_PREDICCION>)', aemet_reports)
     report.tendency.tendencyComment = ' '.join(re_result.group(0).splitlines()[1:])
-    
+
     re_result = re.search('(?<=1\.- Estimación del nivel de peligro:)(?s:.*)(?=2\.- Estado del manto y observaciones recientes)', aemet_reports)
     levels = re_result.group(0).splitlines()
-    
+
     last_region = ''
     region_lines = {}
     for line in levels:
@@ -83,7 +93,7 @@ def get_reports_from_file(aemet_reports):
                 last_region = content[0]
             else:
                 region_lines[last_region] = region_lines[last_region] + line
-    
+
     for elem in region_lines:
         current_report = copy.deepcopy(report)
         current_report.regions.append(RegionType(code_dir[elem.upper()]))
@@ -100,7 +110,6 @@ def get_reports_from_file(aemet_reports):
                 danger_rating2 = None
                 levels = re.findall(r"\((.)\)", sentence)
                 if len(levels) > 1 and not (('por debajo' in sentence) and ('por encima' in sentence)):
-                    print('pm', len(levels) > 1, ('por debajo' in sentence), ('por encima' in sentence))
                     pm_sent = True
                     pm = True
                 if 'por debajo' in sentence:
@@ -126,16 +135,15 @@ def get_reports_from_file(aemet_reports):
                 if danger_rating2 is not None:
                     danger_rating2.set_mainValue_int(int(levels[1]))
                     current_report.dangerRatings.append(danger_rating2)
-                    
-                
+
         if pm:
             pm_report = copy.deepcopy(current_report)
             pm_report.bulletinID = current_report.bulletinID + '_PM'
             current_report.validTime.endTime = current_report.validTime.endTime.replace(hour=12, minute=0)
             pm_report.validTime.startTime = current_report.validTime.endTime
-            
+
             set = False
-            
+
             for danger_rating in pm_report.dangerRatings:
                 if hasattr(danger_rating.elevation, 'upperBound') and pm_ratings_lw != 0:
                     danger_rating.set_mainValue_int(pm_ratings_lw)
@@ -143,12 +151,12 @@ def get_reports_from_file(aemet_reports):
                 if hasattr(danger_rating.elevation, 'lowerBound') and pm_ratings_hi != 0:
                     danger_rating.set_mainValue_int(pm_ratings_hi)
                     set = True
-                    
+
             if not set:
                 pm_report.dangerRatings[0].set_mainValue_int(pm_ge)
-                
+
             reports.append(pm_report)
-                
+
         reports.append(current_report)
 
     return reports
---
+++ tests/test_es_am_pm.py
@@ -0,0 +1,63 @@
+from avacore import pyAvaCore
+from avacore.processor_es import get_reports_from_file
+import unittest
+import xml.etree.ElementTree as ET
+
+
+class TestES(unittest.TestCase):
+
+    def test_ES(self):
+        with open(f'{__file__}.xml', encoding='ISO-8859-1') as f:
+            text = f.read()
+        reports = get_reports_from_file(text)
+        self.assertEqual(len(reports), 10)
+        report = reports[0]
+        self.assertEqual(report.bulletinID, 'ES-NA_2022-02-22 15:57:00+01:00_PM')
+        self.assertEqual(report.publicationTime.isoformat(), '2022-02-22T15:57:00+01:00')
+        self.assertEqual(report.validTime.startTime.isoformat(), '2022-02-23T12:00:00+01:00')
+        self.assertEqual(report.validTime.endTime.isoformat(), '2022-02-23T15:57:00+01:00')
+        self.assertIn('ES-NA', report.get_region_list())
+        self.assertNotIn('ES-GA', report.get_region_list())
+        self.assertEqual(report.dangerRatings[0].mainValue, 'moderate')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'upperBound')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'lowerBound')
+
+        report = reports[1]
+        self.assertEqual(report.validTime.startTime.isoformat(), '2022-02-22T15:57:00+01:00')
+        self.assertEqual(report.validTime.endTime.isoformat(), '2022-02-23T12:00:00+01:00')
+        self.assertEqual(report.dangerRatings[0].mainValue, 'low')
+        self.assertIn('ES-NA', report.get_region_list())
+        self.assertNotIn('ES-GA', report.get_region_list())
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'lowerBound')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'upperBound')
+        self.assertRaises(AttributeError, getattr, report, "predecessor_id")
+
+        report = reports[2]
+        self.assertEqual(report.bulletinID, 'ES-JA_2022-02-22 15:57:00+01:00_PM')
+        self.assertEqual(report.publicationTime.isoformat(), '2022-02-22T15:57:00+01:00')
+        self.assertEqual(report.validTime.startTime.isoformat(), '2022-02-23T12:00:00+01:00')
+        self.assertEqual(report.validTime.endTime.isoformat(), '2022-02-23T15:57:00+01:00')
+        self.assertIn('ES-JA', report.get_region_list())
+        self.assertNotIn('ES-GA', report.get_region_list())
+        self.assertEqual(report.dangerRatings[0].mainValue, 'moderate')
+        self.assertEqual(report.dangerRatings[0].elevation.upperBound, '2400')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'lowerBound')
+        self.assertEqual(report.dangerRatings[1].mainValue, 'moderate')
+        self.assertEqual(report.dangerRatings[1].elevation.lowerBound, '2400')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[1].elevation, 'upperBound')
+
+        report = reports[3]
+        self.assertEqual(report.validTime.startTime.isoformat(), '2022-02-22T15:57:00+01:00')
+        self.assertEqual(report.validTime.endTime.isoformat(), '2022-02-23T12:00:00+01:00')
+        self.assertIn('ES-JA', report.get_region_list())
+        self.assertNotIn('ES-GA', report.get_region_list())
+        self.assertEqual(report.dangerRatings[0].mainValue, 'low')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[0].elevation, 'lowerBound')
+        self.assertEqual(report.dangerRatings[0].elevation.upperBound, '2400')
+        self.assertRaises(AttributeError, getattr, report, "predecessor_id")
+        self.assertEqual(report.dangerRatings[1].mainValue, 'moderate')
+        self.assertEqual(report.dangerRatings[1].elevation.lowerBound, '2400')
+        self.assertRaises(AttributeError, getattr, report.dangerRatings[1].elevation, 'upperBound')
+
+if __name__ == '__main__':
+    unittest.main()
