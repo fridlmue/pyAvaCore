7367ddcfd232a5f9cf1fda8f1f54c5063de4e101
fridlmue <fridl@10hoch-6.de>
2022-03-16 09:14:33 +0100

    remove ratings not requested by region

--- avacore/pyAvaCore.py
+++ avacore/pyAvaCore.py
@@ -1,5 +1,5 @@
 """
-    Copyright (C) 2021 Friedrich Mütschele and other contributors
+    Copyright (C) 2022 Friedrich Mütschele and other contributors
     This file is part of pyAvaCore.
     pyAvaCore is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
