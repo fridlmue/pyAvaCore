58854bdd41e3a0ab0b4a2146b476bbb9f66f5e28
Simon Legner <Simon.Legner@gmail.com>
2022-11-27 23:49:20 +0100

    setup.py: add description

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyAvaCore",
+    description="Library to download and parse EAWS avalanche bulletins",
     version="0.0.0",
     url="https://gitlab.com/albina-euregio/pyAvaCore",
     license="GPL",
