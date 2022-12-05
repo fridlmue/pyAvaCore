a2f383b9d559ca3e2290aa014a3641c786b3f0b4
Simon Legner <Simon.Legner@gmail.com>
2022-12-05 23:07:30 +0100

    processor_se as JsonProcessor

--- avacore/processor_se.py
+++ avacore/processor_se.py
@@ -13,17 +13,8 @@
     along with pyAvaCore. If not, see <http://www.gnu.org/licenses/>.
 """
 import json
-import urllib.request
 from datetime import datetime
-from datetime import time
-import logging
-
-import pytz
-import dateutil.parser
-
-from .avajson import JSONEncoder
-
-from avacore.avabulletin import (
+from .avabulletin import (
     AvaBulletin,
     DangerRating,
     AvalancheProblem,
@@ -31,145 +22,105 @@ from avacore.avabulletin import (
     Region,
     Texts,
 )
+from .avabulletins import Bulletins
+from .processor import JsonProcessor
 
 
-def process_reports_se(region_id, local="sv"):
-    """
-    Downloads and returns requested Avalanche Bulletins
-    """
+class Processor(JsonProcessor):
+    def process_bulletin(self, region_id) -> Bulletins:
+        langkey = "sv"  # Should also work with en and smi (Samiska)
+        url = (
+            "https://nvgis.naturvardsverket.se/geoserver/lavinprognoser/"
+            + "ows?service=WFS&version=1.0.0&request=GetFeature"
+            + "&typeName=lavinprognoser:published_forecasts&outputformat=application/json"
+            + f"&viewparams=lang:{langkey};"
+        )
+        if region_id != "SE":
+            url += f"location:{region_id[3:]};"
+        headers = {"Content-Type": "application/json; charset=utf-8"}
+        response_json = self._fetch_json(url, headers)
+        bulletins = self.parse_json(region_id, response_json)
+        return bulletins
 
-    langkey = "sv" # Should also work with en and smi (Samiska)
+    def parse_json(self, region_id, data) -> Bulletins:
+        # pylint: disable=too-many-locals
+        # pylint: disable=too-many-branches
+        # pylint: disable=too-many-statements
 
-    url = (
-        "http://nvgis.naturvardsverket.se/geoserver/lavinprognoser/"
-        + "ows?service=WFS&version=1.0.0&request=GetFeature"
-        + "&typeName=lavinprognoser:published_forecasts&outputformat=application/json"
-        + f"&viewparams=lang:{langkey};location:{region_id[3:]};"
-    )
+        bulletins = Bulletins()
 
-    url = url + "valid_date:2021-03-01" #ToDo remove line - only debug
+        for feature in data["features"]:
+            properties = feature["properties"]
+            report = AvaBulletin()
+            report.regions.append(Region(region_id))
+            report.publicationTime = datetime.fromisoformat(
+                properties["published_date"]
+            )
+            report.bulletinID = region_id + "_" + str(report.publicationTime)
 
-    headers = {"Content-Type": "application/json; charset=utf-8"}
+            report.validTime.startTime = datetime.fromisoformat(
+                properties["valid_from"]
+            )
+            report.validTime.endTime = datetime.fromisoformat(properties["valid_to"])
 
-    req = urllib.request.Request(url, headers=headers)
+            danger_rating = DangerRating()
+            danger_rating.set_mainValue_int(int(properties["risk"]))
 
-    logging.info("Fetching %s", req.full_url)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
+            report.dangerRatings.append(danger_rating)
 
-    properties = json.loads(content)
+            if properties["problems"]:
+                problems = json.loads(properties["problems"])
+                for problem in problems:
+                    problem_type = problem_ids[problem["problem_type_id"]]
 
-    reports = get_reports_fromjson(region_id, properties)
+                    aspects = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+                    aspect_list = []
 
-    return reports
+                    for i, c in enumerate(aspects):
+                        if problem[aspect_keys[c]]:
+                            aspect_list.append(aspects[i])
 
-
-def process_all_reports_se():
-    """
-    Downloads and returns all norwegian avalanche reports
-    """
-    all_reports = []
-    for region in no_regions:
-        try:
-            m_reports = process_reports_se(region)
-        except Exception as e:  # pylint: disable=broad-except
-            logging.error("Failed to download %s", region, exc_info=e)
-
-        for report in m_reports:
-            all_reports.append(report)
-
-    return all_reports
-
-
-def get_reports_fromjson(region_id, se_report, fetch_time_dependant=True):
-    """
-    Builds the CAAML JSONs form the norwegian JSON formats.
-    """
-    # pylint: disable=too-many-locals
-    # pylint: disable=too-many-branches
-    # pylint: disable=too-many-statements
-
-    reports = []
-    report = AvaBulletin()
-
-    properties = se_report['features'][0]['properties']
-
-    now = datetime.now(pytz.timezone("Europe/Stockholm"))
-
-    report.regions.append(Region(region_id))
-    report.publicationTime = dateutil.parser.parse(
-        properties["published_date"]
-    )
-    report.bulletinID = region_id + "_" + str(report.publicationTime)
-
-    report.validTime.startTime = dateutil.parser.parse(
-        properties["valid_from"]
-    )
-    report.validTime.endTime = dateutil.parser.parse(properties["valid_to"])
-
-    danger_rating = DangerRating()
-    danger_rating.set_mainValue_int(int(properties["risk"]))
-
-    report.dangerRatings.append(danger_rating)
-
-    # up to now unly usage of 3 values found ({6: 'Wind slabs', 3: 'Persistent slabs', 11: 'Wet avalanches', 7: 'Cornices'})
-
-    if not properties["problems"] is None:
-        problems = json.loads(properties["problems"])
-        print(json.dumps(problems, indent=2))
-        for problem in problems:
-            problem_type = problem_ids[problem["problem_type_id"]]
-
-            aspects = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
-            aspect_list = []
-
-            for i, c in enumerate(aspects):
-                if problem[aspect_keys[c]]:
-                    aspect_list.append(aspects[i])
-
-            elev = ""
-            if problem["altitude_meter_above_treeline"] == True:
-                elev = ">treeline"
-            if problem["altitude_meter_below_treeline"] == True:
-                if elev != ">treeline":
-                    elev = "<treeline"
-                else:
                     elev = ""
+                    if problem["altitude_meter_above_treeline"]:
+                        elev = ">treeline"
+                    if problem["altitude_meter_below_treeline"]:
+                        if elev != ">treeline":
+                            elev = "<treeline"
+                        else:
+                            elev = ""
 
-            if not problem_type == "":
-                elevation = Elevation()
-                elevation.auto_select(elev)
-                av_problem = AvalancheProblem()
-                av_problem.aspects = aspect_list
-                av_problem.elevation = elevation
-                av_problem.problemType = problem_type
-                av_problem.terrainFeature = problem["content"]
-                report.avalancheProblems.append(av_problem)
+                    if problem_type:
+                        elevation = Elevation()
+                        elevation.auto_select(elev)
+                        av_problem = AvalancheProblem()
+                        av_problem.aspects = aspect_list
+                        av_problem.elevation = elevation
+                        av_problem.problemType = problem_type
+                        av_problem.terrainFeature = problem["content"]
+                        report.avalancheProblems.append(av_problem)
 
-    wxSynopsis = Texts()
-    avalancheActivity = Texts()
-    snowpackStructure = Texts()
-    travelAdvisory = Texts()
+            wxSynopsis = Texts()
+            avalancheActivity = Texts()
+            snowpackStructure = Texts()
+            travelAdvisory = Texts()
 
-    avalancheActivity.highlights = properties["assessment_title"]
-    avalancheActivity.comment = properties["assessment_content"]
-    snowpackStructure.comment = properties["size_and_spread"]
-    report.tendency.tendencyComment = properties["trend_label"]
-    travelAdvisory.comment = properties["recommendation"]
+            avalancheActivity.highlights = properties["assessment_title"]
+            avalancheActivity.comment = properties["assessment_content"]
+            snowpackStructure.comment = properties["size_and_spread"]
+            report.tendency.tendencyComment = properties["trend_label"]
+            travelAdvisory.comment = properties["recommendation"]
 
-    report.wxSynopsis = wxSynopsis
-    report.avalancheActivity = avalancheActivity
-    report.snowpackStructure = snowpackStructure
-    report.travelAdvisory = travelAdvisory
+            report.wxSynopsis = wxSynopsis
+            report.avalancheActivity = avalancheActivity
+            report.snowpackStructure = snowpackStructure
+            report.travelAdvisory = travelAdvisory
 
-    print(json.dumps(report, cls=JSONEncoder, indent=2))
+            bulletins.append(report)
 
-    reports.append(report)
-
-    return reports
+        return bulletins
 
 
-no_regions = [
+regions = [
     "SE-01",
     "SE-02",
     "SE-03",
@@ -182,19 +133,19 @@ no_regions = [
 ]
 
 problem_ids = {
-    6: 'wind_drifted_snow',
-    3: 'persistent_weak_layers',
-    11: 'wet_snow',
-    7: 'cornice_failure'
+    6: "wind_slab",
+    3: "persistent_weak_layers",
+    11: "wet_snow",
+    7: "cornice_failure",
 }
 
 aspect_keys = {
-    'E': 'direction_meter_east',
-    'N': 'direction_meter_north',
-    'NE': 'direction_meter_north_east',
-    'NW': 'direction_meter_north_west',
-    'S': 'direction_meter_south',
-    'SE': 'direction_meter_south_east',
-    'SW': 'direction_meter_south_west',
-    'W': 'direction_meter_west',
-    }
+    "E": "direction_meter_east",
+    "N": "direction_meter_north",
+    "NE": "direction_meter_north_east",
+    "NW": "direction_meter_north_west",
+    "S": "direction_meter_south",
+    "SE": "direction_meter_south_east",
+    "SW": "direction_meter_south_west",
+    "W": "direction_meter_west",
+}
