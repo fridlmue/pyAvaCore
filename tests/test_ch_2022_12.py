5582cf79118bad8ceb535f1b8cb0744bef2d1e51
Simon Legner <Simon.Legner@gmail.com>
2022-12-08 01:19:41 +0100

    test_ch_2022_12

---
+++ tests/test_ch_2022_12.py
@@ -0,0 +1,15 @@
+from avacore.processor_ch import Processor
+
+import io
+import pathlib
+
+from tests import SnowTest
+
+
+class TestCH(SnowTest):
+    def test_ch_2022(self):
+        processor = Processor()
+        processor.raw_data = io.BytesIO(pathlib.Path(f"{__file__}.zip").read_bytes())
+        processor.year = "2022"
+        bulletins = processor.process_bulletin("")
+        self.assertEqualBulletinJSON(__file__, bulletins)
