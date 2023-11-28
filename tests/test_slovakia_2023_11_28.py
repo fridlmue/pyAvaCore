b77b1424aaa5150e0af9fadf95b079298e756661
Simon Legner <Simon.Legner@gmail.com>
2023-11-28 15:09:35 +0100

    Implement CAAMLv6 processor for SK

--- tests/test_slovakia_2023_11_28.py
+++ tests/test_slovakia_2023_11_28.py
@@ -1,10 +1,10 @@
-from avacore.processor_sk import Processor
+from avacore.processor_caamlv6 import Processor2022
 
 from tests import SnowTest
 
 
 class TestSlovakia(SnowTest):
-    def test_slovakia(self):
-        processor = Processor()
+    def test_slovakia_2023_11_28(self):
+        processor = Processor2022()
         bulletins = processor.parse_json_file("", f"{__file__}.json")
         self.assertEqualBulletinJSON(__file__, bulletins)
