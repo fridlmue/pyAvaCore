0ead638101acf7718b4c510b1de83fe2228ba1dc
Simon Legner <Simon.Legner@gmail.com>
2025-12-06 17:51:19 +0100

    UK: fix cornice_failure to cornices

--- avacore/processor_uk.py
+++ avacore/processor_uk.py
@@ -23,6 +23,7 @@ from avacore.avabulletin import (
     Region,
     Texts,
 )
+from avacore.avabulletin import AvalancheProblemType
 from avacore.avabulletins import Bulletins
 from avacore.processor import JsonProcessor
 
@@ -72,27 +73,27 @@ class Processor(JsonProcessor):
 
             if problems & (1 << 1):
                 problem = AvalancheProblem()
-                problem.problemType = "wind_slab"
+                problem.problemType = AvalancheProblemType.wind_slab
                 report.avalancheProblems.append(problem)
             if problems & (1 << 2):
                 problem = AvalancheProblem()
-                problem.problemType = "persistent_weak_layers"
+                problem.problemType = AvalancheProblemType.persistent_weak_layers
                 report.avalancheProblems.append(problem)
             if problems & (1 << 3):
                 problem = AvalancheProblem()
-                problem.problemType = "new_snow"
+                problem.problemType = AvalancheProblemType.new_snow
                 report.avalancheProblems.append(problem)
             if problems & (1 << 4):
                 problem = AvalancheProblem()
-                problem.problemType = "wet_snow"
+                problem.problemType = AvalancheProblemType.wet_snow
                 report.avalancheProblems.append(problem)
             if problems & (1 << 5):
                 problem = AvalancheProblem()
-                problem.problemType = "cornice_failure"
+                problem.problemType = AvalancheProblemType.cornices
                 report.avalancheProblems.append(problem)
             if problems & (1 << 6):
                 problem = AvalancheProblem()
-                problem.problemType = "gliding_snow"
+                problem.problemType = AvalancheProblemType.gliding_snow
                 report.avalancheProblems.append(problem)
 
             danger_ratings_raw = sais_report["CompassRose"][4:36]
